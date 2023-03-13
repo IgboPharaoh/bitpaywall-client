e5dd12c2aea085e8170f69cebbbed3e7efe6f915
IgboPharaoh <solomoneze62@gmail.com>
2023-03-13 23:17:18 +0100

    add routing to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import { Flex, HStack, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
         <div>
             <Flex ml='80px' mr='80px' gap='64px' justifyContent='space-between'>
                 <HStack width='100%' justifyContent='space-between'>
-                    <Text fontSize='54px' fontWeight='600' color='black' cursor='pointer'>
+                    <Text onClick={() => navigate('/')} fontSize='54px' fontWeight='600' color='black' cursor='pointer'>
                         Mailto
                     </Text>
                 </HStack>
