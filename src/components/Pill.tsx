3d41bbeaf8d28564e695448a878a890025b437ad
IgboPharaoh <solomoneze62@gmail.com>
2023-03-13 20:54:27 +0100

    modified pill text to stop text break

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -4,7 +4,7 @@ import { PillProps } from '../interfaces';
 const Pill = ({ title }: PillProps): JSX.Element => {
     return (
         <Box cursor='pointer' p='4px 12px'  w='fit-content' borderRadius='32px' bgColor='#eaeaea'>
-            <Text fontSize='12px' color='#2d2d2d'>{title}</Text>
+            <Text whiteSpace='nowrap' fontSize='12px' color='#2d2d2d'>{title}</Text>
         </Box>
     );
 };
