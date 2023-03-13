eb32a5ce01f1ce3e5dfd61ee9c21996ca55a5dd6
IgboPharaoh <solomoneze62@gmail.com>
2023-03-13 16:01:03 +0100

    add react router setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <ChakraProvider>
-            <App />
+            <RouterProvider router={router} />
         </ChakraProvider>
     </React.StrictMode>
 );
