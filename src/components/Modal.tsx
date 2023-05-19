669c464d4ac80d6f3dfc6713172d55ba57189c63
IgboPharaoh <solomoneze62@gmail.com>
2023-05-19 08:18:43 +0100

    structure invoice generation with modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,41 +1,56 @@
-import { useState } from 'react';
-import {
-    ModalOverlay,
-    Box,
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    Text,
-    ModalBody,
-    VStack,
-    ModalFooter,
-    HStack,
-    Input,
-    Flex,
-} from '@chakra-ui/react';
-import { ModalProps } from '../interfaces';
-import CustomButton from './CustomButton';
-import QRCode from 'react-qr-code';
+import { useEffect, useState } from 'react';
+import { ModalOverlay, Modal, ModalContent } from '@chakra-ui/react';
+import { ArticleInvoice, ModalProps } from '../interfaces';
 
-const CustomModal = ({ open, onCloseClickCallback, openDrawer, title, description, amount }: ModalProps): JSX.Element => {
+import { genInvoiceApi } from '../api/services';
+import PaymentChecker from './PaymentChecker';
+import PaymentSummary from './PaymentSummary';
+import PaymentInvoice from './PaymentInvoice';
+import { useNavigate } from 'react-router-dom';
+
+const CustomModal = ({ open, onCloseClickCallback, title, description, amount, guid }: ModalProps): JSX.Element => {
+    const linkArr = guid.split('/');
+    const articleId = linkArr[linkArr.length - 1];
+    const userPubKey = 'bc1qgf60crqtlxn7279tgh8lsxzagmu97cyuwtykxwv026s9hwg427fsjvw7uz';
+    const navigate = useNavigate();
+
     const [step, setStep] = useState(0);
-    const [copy, setCopy] = useState('Copy');
+    const [isConfirming, setIsConfirming] = useState(false);
+    const [articleState, setArticleState] = useState<ArticleInvoice>({
+        articleId,
+        amount,
+        userPubKey,
+        hasPaid: false,
+        paymentRequest: '',
+        isPaying: false,
+    });
 
-    const paymentRequest = '234322kjnsjnjsbkjfs';
-    const handleCopyClick = () => {
-        navigator.clipboard.writeText(paymentRequest).then(
-            (value) => {
-                console.log(value);
-            },
-            () => {
-                console.error('failed to copy');
+    if (isConfirming === true) {
+        const interval = setInterval(async () => {
+            console.log('checking invoice state');
+            setIsConfirming(true);
+
+            const invoiceStatus = await fetch('/');
+        }, 5000);
+
+        clearInterval(interval);
+    }
+
+    const { paymentRequest } = articleState;
+    const generateArticleInvoice = async () => {
+        try {
+            const res = await genInvoiceApi(articleId, articleState.hasPaid, amount, userPubKey);
+
+            setArticleState((articleState) => ({ ...articleState, isPaying: false, paymentRequest: res.payReq, hasPaid: res?.article?.hasPaid }));
+
+            console.log({ articleState }, 'article state 2');
+
+            if (res.payReq.length) {
+                setStep(step + 1);
             }
-        );
-        setCopy('Copied');
-        setTimeout(() => {
-            setCopy('Copy');
-        }, 3000);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -45,102 +60,30 @@ const CustomModal = ({ open, onCloseClickCallback, openDrawer, title, descriptio
                 <ModalContent>
                     {step === 0 && (
                         <>
-                            <ModalHeader textTransform='uppercase' fontWeight='700' pb='4px' fontSize='20px'>
-                                Complete Payment
-                            </ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                <VStack alignItems='flex-start' justifyContent='space-between'>
-                                    <Text color='#2d2d2d' fontWeight='500' fontSize='16px'>
-                                        {title}
-                                    </Text>
-                                    <Box>
-                                        <Text
-                                            fontSize='15px'
-                                            borderRadius='32px'
-                                            dangerouslySetInnerHTML={{ __html: `${description.substring(1, 250)}.........` }}
-                                        ></Text>
-                                        <Text fontSize='16px' pt='16px'>
-                                            Interesting right?
-                                        </Text>
-                                        <Text color='#2d2d2d' fontSize='15px'>
-                                            To continue enjoy this article please pay some sats
-                                        </Text>
-                                        <HStack borderTop='1px solid #afabab' borderBottom='1px solid #afabab' mt='16px' pb='8px' pt='8px'>
-                                            <Text fontWeight='600' color='#2d2d2d' fontSize='18px'>
-                                                Amount:
-                                            </Text>
-                                            <Text fontWeight='700' color='#2d2d2d' fontSize='18px'>
-                                                {amount} sats
-                                            </Text>
-                                        </HStack>
-                                    </Box>
-                                </VStack>
-                            </ModalBody>
-                            <ModalFooter>
-                                <CustomButton backgroundColor='#000000' onClick={() => setStep(1)}>
-                                    Pay
-                                </CustomButton>
-                            </ModalFooter>
+                            <PaymentSummary title={title} description={description} amount={amount} generateArticleInvoice={generateArticleInvoice} />
                         </>
                     )}
+
                     {step === 1 && (
                         <>
-                            <ModalHeader textTransform='uppercase' fontWeight='700' pb='8px' fontSize='20px'>
-                                Pay with lightning
-                            </ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                <Flex width='100%' flexDir='column' alignItems='center'>
-                                    <QRCode height='100%' width='100%' value={paymentRequest} />
-                                    <HStack spacing='16px' pt='16px'>
-                                        <Input
-                                            bgColor='#eaeaea'
-                                            color='black'
-                                            fontWeight='500'
-                                            placeholder={paymentRequest}
-                                            defaultValue={paymentRequest}
-                                            readOnly
-                                            height='42px'
-                                        />
-                                        <CustomButton color='black' border='1px solid black' backgroundColor='transparent' onClick={handleCopyClick}>
-                                            {copy}
-                                        </CustomButton>
-                                    </HStack>
-                                </Flex>
-                                <HStack
-                                    justifyContent='center'
-                                    alignItems='center'
-                                    borderTop='1px solid #afabab'
-                                    borderBottom='1px solid #afabab'
-                                    mt='16px'
-                                    pb='8px'
-                                    pt='8px'
-                                >
-                                    <Text fontWeight='600' color='#2d2d2d' fontSize='18px'>
-                                        Amount:
-                                    </Text>
-                                    <Text fontWeight='700' color='#2d2d2d' fontSize='18px'>
-                                        {amount} sats
-                                    </Text>
-                                </HStack>
-                            </ModalBody>
-                            <ModalFooter justifyContent='space-between'>
-                                <Text
-                                    _hover={{ textDecor: 'underline' }}
-                                    onClick={() => setStep(0)}
-                                    fontWeight='500'
-                                    color='#2d2d2d'
-                                    cursor='pointer'
-                                    textTransform='uppercase'
-                                    fontSize='15px'
-                                >
-                                    Back
-                                </Text>
-                                <CustomButton backgroundColor='#000000' onClick={openDrawer}>
-                                    Complete
-                                </CustomButton>
-                            </ModalFooter>
+                            <PaymentInvoice
+                                back={() => setStep(0)}
+                                confirmPayment={() => setStep(2)}
+                                paymentRequest={paymentRequest}
+                                amount={amount}
+                            />
+                        </>
+                    )}
+
+                    {step === 2 && (
+                        <>
+                            <PaymentChecker
+                                back={() => setStep(1)}
+                                isConfirming={isConfirming}
+                                navigateToArticle={() => {
+                                    navigate(articleId, { state: { title, description } });
+                                }}
+                            />
                         </>
                     )}
                 </ModalContent>
